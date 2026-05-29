567c8bdf5df83330f2b9715a3a185ae75309ad24
r0b1nv41kl4 <robin.vaikla@ametikool.ee>
2026-05-29 12:46:15 +0300

    Configured ESlint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    ...js.configs.recommended,
+  },
+];
