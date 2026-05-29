5dbc87eb53b7a66cbdb56fe2cf5f04a2ffbdb487
r0b1nv41kl4 <robin.vaikla@ametikool.ee>
2026-05-29 13:34:22 +0300

    Installed ReactRouter (declarative)

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import App from './app';
+
+const root = document.getElementById('root');
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
 const appName = import.meta.env.VITE_APP_NAME;
 const apiUrl = import.meta.env.VITE_API_URL;
 
